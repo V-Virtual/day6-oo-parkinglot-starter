0f87c8c60f63296f9d4ec64ebff08a49750be890
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-08 19:43:27 +0800

    test and feat: parking and fetch

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,35 @@
 package com.afs.parkinglot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ParkingLot {
+    private int capacity;
+    private Map<Ticket, Car> parkedCars = new HashMap<Ticket, Car>();
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Ticket parking(Car car){
+        if(parkedCars.size() >= capacity) {
+            throw new RuntimeException("Not enough position.");
+        }
+        Ticket ticket = new Ticket();
+        parkedCars.put(ticket, car);
+        return ticket;
+    }
+
+    public Car fetch(Ticket ticket){
+        if(!parkedCars.containsKey(ticket)) {
+            throw new RuntimeException("Unrecognized parking ticket.");
+        }
+        return parkedCars.remove(ticket);
+    }
+
+    public Map<Ticket, Car> getParkedCars() {
+        return parkedCars;
+    }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,30 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ParkingLotTest {
+
+    @Test
+    public void should_return_ticket_given_parking_lot_and_car_when_parking(){
+        //Gavin
+        ParkingLot parkingLot = new ParkingLot(20);
+        Car car = new Car();
+        //When
+        parkingLot.parking(car);
+        //Then
+        assertEquals(1, parkingLot.getParkedCars().size());
+    }
+
+    @Test
+    public void should__given_parking_lot_and_car_when_without_position(){
+        //Given
+        ParkingLot parkingLot = new ParkingLot(20);
+        Car car = new Car();
+        Ticket ticket = parkingLot.parking(car);
+        //When and Then
+        assertEquals(car, parkingLot.fetch(ticket));
+    }
 }
